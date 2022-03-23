8b5ead24ee61a45557543eabb739594812e5eaae
Jhun Louie Gonzales <jhun@bizbox.ph>
2022-03-23 16:46:05 +0800

    Refix Not Creating Document in Counter

--- biz-counter.js
+++ biz-counter.js
@@ -1,20 +1,38 @@
 import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
 
 const getCollection = (collection) => Mongo.Collection.get(collection);
 
 export const incrementCounter = (collection, selector, count = 1) => {
+    check(collection, String);
+    check(selector, String);
+    check(count, Number);
+
     const collec = getCollection(collection);
-    const upsert = Meteor.wrapAsync(collec.upsert, collec);
-    upsert({ _id: selector }, { $inc: { next_val: count } });
+    const upsert = Meteor.wrapAsync(collec.update, collec);
+
+    upsert(
+        { _id: selector }, 
+        { $inc: { next_val: count }},
+        { upsert: true }
+    );
 
     const result = collec.findOne({ _id: selector });
     return result?.value?.next_val || result?.next_val;
 };
 
 export const setCounter = (collection, selector, counter) => {
+    check(collection, String);
+    check(selector, String);
+    check(counter, Number);  
+
     const collec = getCollection(collection);
     const update = Meteor.wrapAsync(collec.update, collec);
 
-    update({ _id: selector }, { $set: { next_val: counter } });
+    update(
+        { _id: selector }, 
+        { $set: { next_val: counter } },
+        { upsert: true }
+    );
 };
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jhunlouiegonzales:biz-counter',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'Atomic counters stored in MongoDB',
   git: '',
   documentation: 'README.md',
